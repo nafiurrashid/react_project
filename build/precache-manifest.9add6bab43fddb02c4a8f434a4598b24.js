b137486c1bc5841b70158761d4a506703c664448
Nafiur Rashid <nafiurrashid@gmail.com>
2022-05-09 15:10:43 +0600

    minor search design

---
+++ build/precache-manifest.9add6bab43fddb02c4a8f434a4598b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "57adf0107e9cbae552e0",
+    "url": "/static/css/main.4f74a778.chunk.css"
+  },
+  {
+    "revision": "57adf0107e9cbae552e0",
+    "url": "/static/js/main.57adf010.chunk.js"
+  },
+  {
+    "revision": "38ecc02d009634cff82d",
+    "url": "/static/js/1.38ecc02d.chunk.js"
+  },
+  {
+    "revision": "ddf822357378248fa4a3",
+    "url": "/static/css/2.b06d08d6.chunk.css"
+  },
+  {
+    "revision": "ddf822357378248fa4a3",
+    "url": "/static/js/2.ddf82235.chunk.js"
+  },
+  {
+    "revision": "e3e46466794921c6291d",
+    "url": "/static/js/runtime~main.e3e46466.js"
+  },
+  {
+    "revision": "9b3ee205615f94bf3a3bf67611db2a9e",
+    "url": "/index.html"
+  }
+];
