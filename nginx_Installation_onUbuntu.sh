0bb150d5cc1f3dcf134c899250878ce7fe5169c5
Nafiur Rashid <nafiurrashid@gmail.com>
2025-06-03 09:08:15 +0600

    Update nginx_Installation_onUbuntu.sh

--- nginx_Installation_onUbuntu.sh
+++ nginx_Installation_onUbuntu.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#@Nafiur_Rashid
+
+#  @author ${Nafiur Rashid}
+#  @email ${nafiurrashid@gmail.com}
+#  ${Version: 1.00}
+  
 set -e
 
 echo "🔄 Updating package index..."
@@ -14,12 +18,33 @@ sudo systemctl enable nginx
 echo "🚀 Starting Nginx service..."
 sudo systemctl start nginx
 
-echo "🚀 Current status of Nginx service..."
+echo " Current status of Nginx service..."
 sudo systemctl status nginx --no-pager
 
 echo "✅ Nginx is installed and running!"
 echo "🌐 Visit: http://localhost or http://<your-server-ip>"
-```
+
+echo "📦 Installing npm..."
+sudo apt install npm -y
+
+echo "📦 Cloning this repo..."
+git clone https://github.com/nafiurrashid/react_project.git
+cd react_project
+
+echo "📦 creating build file.."
+npm install
+npm run build
+
+echo "📦 moving the buildfile.."
+sudo rm /var/www/html/index.nginx-debian.html
+sudo cp -r build/* /var/www/html/
+
+echo "🔄 Relaod & restart Nginx..."
+sudo systemctl reload nginx
+sudo systemctl restart nginx
+
+
+
 
 
 
